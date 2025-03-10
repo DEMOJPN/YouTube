a80f2313cff5c2267a569500ac22610837244282
DemoJPN <89500765+DEMOJPN@users.noreply.github.com>
2025-03-10 18:37:59 +0900

    Updated ktmr_selectcc_base.gd

--- ktmr_selectcc_base.gd
+++ ktmr_selectcc_base.gd
@@ -2,17 +2,20 @@
 
 # Node Structure:
 #	- root
-#	     - Pivot (Node3D or CSGSphere3D Scaled to 0.1 on XYZ, Rotated -90 degrees on Z)
-#		     - CSGSphere3D (Position set to -10m on X)
-#	     	     - Label3D (Text set to "^", font size 100 and Position set to -1 on X, Rotated 90 degrees on Y and -180 degrees on Z)
-#	     - Camera3D (Set to Current)
+#	  - CSGSphere3D (Position: .0.4, 1, 0 // Rotation: 0, 0, -90 // Scale 0.1, 0.1, 0.1)
+#	    - Label3D (Text: ^ // Font Siz: 100 // Position: -1, 0, 0 // Rotation: 0, -90, -180)
+#     - Camera3D (Position: 0, 2, 0 // Rotation: -90, -90, 0)
 
 extends Node3D
 
-# Variables
-@onready var camera: Camera3D = $Pivot/Camera3D
-@onready var player_character: CSGSphere3D = $Pivot/CSGSphere3D
 
+# Variables
+@onready var camera: Camera3D = $Camera3D
+@onready var player_character: CSGSphere3D = $CSGSphere3D
+@onready var pivot: Node3D = $"."
+
+var input = Vector2.ZERO
+var speed : float = 0.005
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,34 +23,80 @@ func _ready() -> void:
 	pass
 
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	update_character_position()
 
+
+# Rotates the character around a center pivot.
+func update_character_position():
+	# Right
+	if input == Vector2(1,0):
+		setCharacterRight()
+		pivot.transform = pivot.transform.rotated_local(Vector3(1.0, 0.0, 0.0).normalized(), speed)
+	
+	# Left
+	if input == Vector2(-1,0):
+		setCharacterLeft()
+		pivot.transform = pivot.transform.rotated_local(Vector3(-1.0, 0.0, 0.0).normalized(), speed)
+	
+	# Down
+	if input == Vector2(0,1):
+		setCharacterDown()
+		pivot.transform = pivot.transform.rotated_local(Vector3(0.0, 0.0, 1.0).normalized(), speed)
+	
+	# Up
+	if input == Vector2(0,-1):
+		setCharacterUp()
+		pivot.transform = pivot.transform.rotated_local(Vector3(0.0, 0.0, -1.0).normalized(), speed)
+
+	# Up and Right
+	if input.x <= 0.8  and input.x >= 0.7 and input.y >= -0.8 and input.y <= -0.7:
+		setCharacterUpRight()
+		pivot.transform = pivot.transform.rotated_local(Vector3(1.0, 0.0, -1.0).normalized(), speed)
+	
+	# Up and Left
+	if input.x >= -0.8  and input.x <= -0.7 and input.y >= -0.8 and input.y <= -0.7:
+		setCharacterUpLeft()
+		pivot.transform = pivot.transform.rotated_local(Vector3(-1.0, 0.0, -1.0).normalized(), speed)
+	
+	# Down and Right
+	if input.x <= 0.8  and input.x >= 0.7 and input.y <= 0.8 and input.y >= 0.7:
+		setCharacterDownRight()
+		pivot.transform = pivot.transform.rotated_local(Vector3(1.0, 0.0, 1.0).normalized(), speed)
+	
+	# Down and Left
+	if input.x >= -0.8  and input.x <= -0.7 and input.y <= 0.8 and input.y >= 0.7:
+		setCharacterDownLeft()
+		pivot.transform = pivot.transform.rotated_local(Vector3(-1.0, 0.0, 1.0).normalized(), speed)
+
+	# Placeholder for animation
+	if input == Vector2(0,0):
+		pass
 
 
 # 8 Direction Character Rotation
 func setCharacterUp() -> void:
-	player_character.rotation.x =  deg_to_rad(180.0)
+	player_character.rotation.y =  deg_to_rad(180.0)
 
 func setCharacterDown() -> void:
-	player_character.rotation.x = deg_to_rad(0.0)
+	player_character.rotation.y = deg_to_rad(0.0)
 
 func setCharacterLeft() -> void:
-	player_character.rotation.x = deg_to_rad(90.0)
+	player_character.rotation.y = deg_to_rad(-90.0)
 
 func setCharacterRight() -> void:
-	player_character.rotation.x = deg_to_rad(-90.0)
+	player_character.rotation.y = deg_to_rad(90.0)
 
 func setCharacterUpLeft() -> void:
-	player_character.rotation.x = deg_to_rad(135.0)
+	player_character.rotation.y = deg_to_rad(-135.0)
 
 func setCharacterDownLeft() -> void:
-	player_character.rotation.x = deg_to_rad(45.0)
+	player_character.rotation.y = deg_to_rad(-45.0)
 
 func setCharacterUpRight() -> void:
-	player_character.rotation.x = deg_to_rad(-135.0)
+	player_character.rotation.y = deg_to_rad(135.0)
 
 func setCharacterDownRight() -> void:
-	player_character.rotation.x = deg_to_rad(-45.0)
+	player_character.rotation.y = deg_to_rad(45.0)
